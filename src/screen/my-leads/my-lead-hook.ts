775d71c4e695e710a74218cd8930fd17d1fa7197
Rohit Aware <rohit.a@mypcot.com>
2024-11-13 10:36:18 +0530

    fixes

---
+++ src/screen/my-leads/my-lead-hook.ts
@@ -0,0 +1,79 @@
+import React from 'react';
+import { getHashString } from '../../utility/hashing';
+import { useAuthStore, useMyLeadStore } from '../../store';
+
+
+const useMyLeadHook = () => {
+
+    const { user_detail: userData, deviceId: uuid, token } = useAuthStore();
+    const { getLeads, isFinish, leadsData, page, loading, setMyLeadPage } = useMyLeadStore();
+    const [search, setSearch] = React.useState('');
+    const [refresh, setRefresh] = React.useState(false);
+
+    const fetchLeads = async ({ page = 0 }: { page?: number }) => {
+        console.log("Initial fetch with page:", page);
+        try {
+            const fnName = 'getLeads';
+            const hash_key = getHashString(
+                userData.mkey!,
+                userData.msalt!,
+                uuid,
+                fnName,
+            );
+            const formData = new FormData();
+            formData.append('uuid', uuid);
+            formData.append('hash_key', hash_key);
+            formData.append('page_number', page);
+            formData.append('limit', 2);
+            formData.append('search_key', search);
+            getLeads({ token, formData, leadPage: page })
+        } catch (error: any) {
+            console.log("error isnide fetchLead : ", error);
+        }
+        setRefresh(false);
+    };
+
+    const onEndReached = async () => {
+        if (!isFinish) {
+            setMyLeadPage({ leadPage: page + 1 });
+            fetchLeads({ page: page + 1 });
+        }
+    };
+
+    const onFilter = () => {
+        setSearch('');
+        // navigation.navigate('FilterMyLeads');
+    };
+
+    const onRefresh = () => {
+        setRefresh(true);
+        setMyLeadPage({ leadPage: 0 });
+        fetchLeads({});
+    };
+
+    React.useEffect(() => {
+        setMyLeadPage({ leadPage: 0 });
+        fetchLeads({ page: 0 });
+
+        const timeoutId = setTimeout(() => {
+            setMyLeadPage({ leadPage: 1 });
+        }, 1000);
+
+        return () => clearTimeout(timeoutId);
+    }, []);
+
+
+    return {
+        page,
+        search,
+        loading,
+        refresh,
+        isFinish,
+        onFilter,
+        onRefresh,
+        leadsData,
+        setSearch,
+        onEndReached,
+    };
+};
+export { useMyLeadHook };
--- src/screen/my-leads/my-lead-screen.tsx
+++ src/screen/my-leads/my-lead-screen.tsx
@@ -1,71 +1,25 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Colors } from '../../constants';
+import { useMyLeadHook } from './my-lead-hook';
 import LeadsList from './components/leads-list';
 import { Header, SearchBar } from '../../components';
-import { getHashString } from '../../utility/hashing';
-import { NavigationProp, useNavigation } from '@react-navigation/native';
-import { useAuthStore, useMyLeadStore } from '../../store';
-import { MainStackProps } from '../../router/interface';
-import { Colors } from '../../constants';
+
 
 const MyLeads = () => {
-    const { user_detail: userData, deviceId: uuid, token } = useAuthStore();
-    const { getLeads, isFinish, leadsData, page, loading, setMyLeadPage } = useMyLeadStore();
-    const navigation = useNavigation<NavigationProp<MainStackProps, 'BottomTab'>>();
-    const [search, setSearch] = React.useState('');
-    const [refresh, setRefresh] = React.useState(false);
+    const {
+        page,
+        search,
+        loading,
+        refresh,
+        isFinish,
+        onFilter,
+        onRefresh,
+        leadsData,
+        setSearch,
+        onEndReached,
+    } = useMyLeadHook();
 
-    const fetchLeads = async ({ page = 0 }: { page?: number }) => {
-        console.log("Initial fetch with page:", page);
-        try {
-            const fnName = 'getLeads';
-            const hash_key = getHashString(
-                userData.mkey!,
-                userData.msalt!,
-                uuid,
-                fnName,
-            );
-            const formData = new FormData();
-            formData.append('uuid', uuid);
-            formData.append('hash_key', hash_key);
-            formData.append('page_number', page);
-            formData.append('limit', 2);
-            formData.append('search_key', search);
-            getLeads({ token, formData, leadPage: page })
-        } catch (error: any) {
-            console.log("error isnide fetchLead : ", error);
-        }
-        setRefresh(false);
-    };
-
-    const onEndReached = async () => {
-        if (!isFinish) {
-            setMyLeadPage({ leadPage: page + 1 });
-            fetchLeads({ page: page + 1 });
-        }
-    };
-
-    const onFilter = () => {
-        setSearch('');
-        // navigation.navigate('FilterMyLeads');
-    };
-
-    const onRefresh = () => {
-        setRefresh(true);
-        setMyLeadPage({ leadPage: 0 });
-        fetchLeads({});
-    };
-
-    React.useEffect(() => {
-        setMyLeadPage({ leadPage: 0 });
-        fetchLeads({ page: 0 });
-
-        const timeoutId = setTimeout(() => {
-            setMyLeadPage({ leadPage: 1 });
-        }, 1000);
-
-        return () => clearTimeout(timeoutId);
-    }, []);
 
     return (
         <View style={{ flex: 1, backgroundColor: Colors.white }}>
@@ -87,6 +41,6 @@ const MyLeads = () => {
             />
         </View>
     )
-}
+};
 
 export default MyLeads;
